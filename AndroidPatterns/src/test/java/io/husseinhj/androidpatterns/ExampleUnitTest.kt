e4d7c4967445693b69a9f4efa81cacf44e029fff
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2021-10-03 12:20:46 +0330

    Ready for github package manager

--- AndroidPatterns/src/test/java/io/husseinhj/androidpatterns/ExampleUnitTest.kt
+++ AndroidPatterns/src/test/java/io/husseinhj/androidpatterns/ExampleUnitTest.kt
@@ -10,8 +10,58 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private val simpleCommaPattern = "##,#,#"
+    private val simpleDashPattern = "##-#-#"
+    private val mobilePattern = "98###(###)## ##"
+    private val postalCodePattern = "#####-#####"
+    private val simpleDashPatternWithEndedZero = "##-#-#0.00"
+
+    // String
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun applyPatternForStringTypeWithComma() {
+        assertEquals("12345".applyPattern(simpleCommaPattern), "12,3,45")
     }
+    @Test
+    fun applyExactPatternForStringTypeWithComma() {
+        assertEquals("12345".applyPattern(simpleCommaPattern, true), "12,3,4")
+    }
+    @Test
+    fun applyPatternForSimpleString() {
+        assertEquals("12345".applyPattern(simpleDashPattern), "12-3-45")
+    }
+    @Test
+    fun applyExactPatternForStringHasHardCodedZero() {
+        assertEquals("12345".applyPattern(simpleDashPatternWithEndedZero, exactPattern = true), "12-3-40.00")
+    }
+    @Test
+    fun applyMobilePattern() {
+        assertEquals("9121234567".applyPattern(mobilePattern, exactPattern = true), "98912(123)45 67")
+    }
+    @Test
+    fun applyPostalCodePattern() {
+        assertEquals("4771865131".applyPattern(postalCodePattern, exactPattern = true), "47718-65131")
+    }
+
+    // Int
+    @Test
+    fun applyPatternForIntTypeWithComma() {
+        assertEquals(12345.applyPattern(simpleCommaPattern), "12,3,45")
+    }
+    @Test
+    fun applyExactPatternForIntTypeWithComma() {
+        assertEquals(12345.applyPattern(simpleCommaPattern, true), "12,3,4")
+    }
+    @Test
+    fun applyPatternForIntSimplePattern() {
+        assertEquals(12345.applyPattern(simpleDashPattern), "12-3-45")
+    }
+    @Test
+    fun applyExactPatternForIntHasHardCodedZero() {
+        assertEquals(12345.applyPattern(simpleDashPatternWithEndedZero, exactPattern = true), "12-3-40.00")
+    }
+    @Test
+    fun applyIntMobilePattern() {
+        assertEquals(9121234567.applyPattern(mobilePattern, exactPattern = true), "98912(123)45 67")
+    }
+
 }
